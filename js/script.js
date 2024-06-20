75935a140919b20fb34535f3d494d41f38e4956e
WSNN124 <sonwin61@gmail.com>
2024-06-20 19:34:23 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active hamburger menu
 
 const navbarNav = document.querySelector('.navbar-nav');
 
@@ -9,12 +9,30 @@ onclick = () => {
     
 }
 
-// klik di luar sidebar untuk menghilangkan nav
+// klik di luar elemen untuk menghilangkan nav
 
-const hamburger = document.querySelector('#hamburger-menu');
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click',function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
 })
+
+document.addEventListener('click',function(e) {
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+})
+
+// Toggle class acrive search form
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}

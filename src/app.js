8491ed123dcc407c989ed5740804844e3f5d1ce1
WSNN124 <sonwin61@gmail.com>
2024-06-27 10:55:24 +0700

    fourth commit

--- src/app.js
+++ src/app.js
@@ -45,30 +45,89 @@ document.addEventListener("alpine:init", () => {
 
       const cartItem = this.items.find((item) => item.id === id);
 
-    //   jika item lebih dari 1
-    if(cartItem.quantity > 1) {
+      //   jika item lebih dari 1
+      if (cartItem.quantity > 1) {
         // telusuri 1 1
         this.items = this.items.map((item) => {
-            // jika buka barang yang diklik
-            if(item.id !== id) {
-                return item;
-            } else {
-                item.quantity--;
-                item.total = item.price * item.quantity;
-                this.quantity--;
-                this.total -= item.price;
-                return item;
-            }
-        })
-    } else if (cartItem.quantity === 1) {
+          // jika buka barang yang diklik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
         // jika barangnya sisa 1
         this.items = this.items.filter((item) => item.id !== id);
         this.quantity--;
         this.total -= cartItem.price;
-    }
+      }
     },
   });
 });
+
+// Form Validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// Kirim data ketika tombol checkout diklik
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  // const message = formatMessage(objData);
+  // window.open("http://wa.me/6282392096228?text=" + encodeURIComponent(message));
+
+  // minta transaction token menggunakan ajax/fetch
+
+  try {
+    const response = await fetch("php/placeOrder.php", {
+      method: "POST",
+      body: data,
+    });
+    const token = await response.text();
+
+    window.snap.pay(token);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// format pesan whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  No HP: ${obj.phone}
+  Data Pesanan
+  ${JSON.parse(obj.items).map(
+    (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)})\n`
+  )}
+  TOTAL: ${rupiah(obj.total)}
+  Terima kasih.`;
+};
 
 // Konversi ke Rupiah
 const rupiah = (number) => {
